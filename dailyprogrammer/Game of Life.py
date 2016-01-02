95ea0c964c02570b68396c005fd82534031dad9b
André Vennberg <andre.vennberg@gmail.com>
2016-01-02 23:50:45 +0100

    Updated game of life, and made it way quicker

--- dailyprogrammer/Game of Life.py
+++ dailyprogrammer/Game of Life.py
@@ -1,10 +1,13 @@
 import sys
+from time import clock
 
 import pygame
 
 from math import floor
 
 screen = None
+
+active_cells = set()
 
 
 def new_array(height, width):
@@ -35,14 +38,6 @@ def click_handler(pos, board, scale, button):
         toggle_cell(cell_x, cell_y, scale, 1)
 
 
-def draw_grid(width, height, scale):
-    screen.fill((255, 255, 255))
-
-    for y in range(height):
-        for x in range(width):
-            pygame.draw.rect(screen, (0, 0, 0), (x * scale, y * scale, scale, scale), 1)
-
-
 def cell_check(board, width, height, infinite):
     new_board = new_array(height, width)
 
@@ -53,42 +48,88 @@ def cell_check(board, width, height, infinite):
         x_range = range(1, width - 1)
         y_range = range(1, height - 1)
 
-    for y in y_range:
-        for x in x_range:
+    if not active_cells:
+        for y in y_range:
+            for x in x_range:
+                if x == 0:
+                    x_vals = [width - 1, 0, 1]
+                elif x == width - 1:
+                    x_vals = [x - 1, x, 0]
+                else:
+                    x_vals = [x - 1, x, x + 1]
+
+                if y == 0:
+                    y_vals = [height - 1, 0, 1]
+                elif y == height - 1:
+                    y_vals = [y - 1, y, 0]
+                else:
+                    y_vals = [y - 1, y, y + 1]
+
+                field_sum = sum([board[a][b] for a in y_vals for b in x_vals])
+                old_value = board[y][x]
+
+                if field_sum == 3:
+                    new_board[y][x] = 1
+                    active_cells.add((x, y))
+                elif field_sum != 4:
+                    new_board[y][x] = 0
+                else:
+                    new_board[y][x] = old_value
+    else:
+        for x, y in active_cells.copy():
             if x == 0:
-                x_vals = [width - 1, 0, 1]
+                x_neighbors = [width - 1, 0, 1]
             elif x == width - 1:
-                x_vals = [x - 1, x, 0]
+                x_neighbors = [x - 1, x, 0]
             else:
-                x_vals = [x - 1, x, x + 1]
+                x_neighbors = [x - 1, x, x + 1]
 
             if y == 0:
-                y_vals = [height - 1, 0, 1]
+                y_neighbors = [height - 1, 0, 1]
             elif y == height - 1:
-                y_vals = [y - 1, y, 0]
+                y_neighbors = [y - 1, y, 0]
             else:
-                y_vals = [y - 1, y, y + 1]
+                y_neighbors = [y - 1, y, y + 1]
 
-            neighbor_sum = sum([board[a][b] for a in y_vals for b in x_vals]) - board[y][x]
+            for cx in x_neighbors:
+                for cy in y_neighbors:
+                    if cx == 0:
+                        x_vals = [width - 1, 0, 1]
+                    elif cx == width - 1:
+                        x_vals = [cx - 1, cx, 0]
+                    else:
+                        x_vals = [cx - 1, cx, cx + 1]
 
-            if board[y][x] == 1 and 3 < neighbor_sum < 2:
-                new_board[y][x] = 0
-            elif board[y][x] == 1 and (neighbor_sum == 2 or neighbor_sum == 3):
-                new_board[y][x] = 1
-            elif board[y][x] == 0 and neighbor_sum == 3:
-                new_board[y][x] = 1
+                    if cy == 0:
+                        y_vals = [height - 1, 0, 1]
+                    elif cy == height - 1:
+                        y_vals = [cy - 1, cy, 0]
+                    else:
+                        y_vals = [cy - 1, cy, cy + 1]
+
+                    field_sum = sum([board[a][b] for a in y_vals for b in x_vals])
+                    old_value = board[cy][cx]
+
+                    if field_sum == 3:
+                        new_board[cy][cx] = 1
+
+                        if (cx, cy) not in active_cells:
+                            active_cells.add((cx, cy))
+
+                    elif field_sum != 4:
+                        active_cells.discard((cx, cy))
+                    else:
+                        new_board[cy][cx] = old_value
 
     return new_board
 
 
-def draw_image(board, scale):
-    draw_grid(len(board[0]), len(board), scale)
+def draw_image(scale):
+    screen.fill((255, 255, 255))
 
-    for y in range(len(board)):
-        for x in range(len(board[y])):
-            if board[y][x] == 1:
-                rect = (x * scale, y * scale, scale, scale)
-                screen.fill((0, 0, 0), rect=rect)
+    for x, y in active_cells:
+        rect = (x * scale, y * scale, scale, scale)
+        screen.fill((0, 0, 0), rect=rect)
 
     pygame.display.flip()
 
@@ -105,7 +146,7 @@ def game(x, y, steps, delay, scale, infinite):
     old_mouse_pos = (-1, -1)
 
     board = cell_check(board, x, y, infinite)
-    draw_image(board, scale)
+    draw_image(scale)
 
     while True:
         for event in pygame.event.get():
@@ -140,8 +181,13 @@ def game(x, y, steps, delay, scale, infinite):
                     current_step += 1
                     last = now
 
+                    t1 = clock()
                     board = cell_check(board, x, y, infinite)
-                    draw_image(board, scale)
+                    draw_image(scale)
+                    t2 = clock()
+
+                    print("Loop time: " + str((t2 - t1) * 1000) + " ms")
+                    print("Active cells: " + str(len(active_cells)))
 
 
 def start(x, y, steps, delay, scale, infinite=True):
@@ -149,7 +195,8 @@ def start(x, y, steps, delay, scale, infinite=True):
 
     global screen
     screen = pygame.display.set_mode((x * scale, y * scale))
+    screen.fill((255, 255, 255))
 
     game(x, y, steps, delay, scale, infinite)
 
-start(50, 50, 0, 100, 20)
+start(1000, 1000, 0, 0, 1)
