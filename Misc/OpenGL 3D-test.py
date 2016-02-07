e658f33279ec4fbe47d10973a8043eaee5025025
André Vennberg <andre.vennberg@gmail.com>
2016-02-07 21:09:21 +0100

    Continued work on 3D framework

--- Misc/OpenGL 3D-test.py
+++ Misc/OpenGL 3D-test.py
@@ -2,7 +2,73 @@ import pyglet
 from pyglet.gl import *
 from pyglet.window import key
 
+vertex_vbo = None
+color_vbo = None
+
 fps_display = pyglet.clock.ClockDisplay()
+
+cube_signs = [
+    (-1, 1, -1), (-1, 1, 1), (1, 1, 1), (1, 1, -1),         # Top
+    (-1, -1, -1), (1, -1, -1), (1, -1, 1), (-1, -1, 1),     # Bottom
+    (-1, -1, -1), (-1, -1, 1), (-1, 1, 1), (-1, 1, -1),     # Left
+    (1, -1, 1), (1, -1, -1), (1, 1, -1), (1, 1, 1),         # Right
+    (-1, -1, 1), (1, -1, 1), (1, 1, 1), (-1, 1, 1),         # Front
+    (1, -1, -1), (-1, -1, -1), (-1, 1, -1), (1, 1, -1)      # Back
+]
+
+cube_vertices = []
+cube_colors = []
+
+cube_indexes = {}
+cube_translations = {}
+
+
+def init():
+    global vertex_vbo, color_vbo
+
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glDepthFunc(GL_LEQUAL)
+
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    glClearDepth(1.0)
+
+    glEnable(GL_DEPTH_TEST)
+    glDepthFunc(GL_LEQUAL)
+    glShadeModel(GL_SMOOTH)
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
+
+    add_cube(1, (0.0, 0.0, 0.0), (1000.0, 1.0, 1000.0), (1.0, 1.0, 1.0, 1.0))
+    add_cube(2, (0.0, 2.0, 0.0), (1.0, 1.0, 1.0), (1.0, 0.0, 0.0, 1.0))
+
+    vertex_vbo = VBO()
+    vertex_vbo.data(cube_vertices)
+
+    color_vbo = VBO()
+    color_vbo.data(cube_colors)
+
+
+class VBO:
+    def __init__(self):
+        self.buffer = GLuint(0)
+        glGenBuffers(1, self.buffer)
+
+    def data(self, data):
+        data_gl = (GLfloat * len(data))(*data)
+
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffer)
+        glBufferData(GL_ARRAY_BUFFER, len(data) * 4, data_gl, GL_STATIC_DRAW)
+
+    def bind(self):
+        glBindBuffer(GL_ARRAY_BUFFER, self.buffer)
+
+    def vertex(self):
+        self.bind()
+        glVertexPointer(3, GL_FLOAT, 0, 0)
+
+    def color(self):
+        self.bind()
+        glColorPointer(4, GL_FLOAT, 0, 0)
 
 
 class Camera(object):
@@ -16,8 +82,6 @@ class Camera(object):
     def view(self, width, height):
         self.w, self.h = width, height
         glViewport(0, 0, width, height)
-
-        print("Viewport " + str(width) + "x" + str(height))
 
         if self.mode == 1:
             self.perspective()
@@ -38,12 +102,10 @@ class Camera(object):
 
     def key(self, symbol, modifiers):
         if symbol == key.F1:
-            print("Projection: 3D Perspective")
             self.mode = 1
             self.perspective()
 
         elif symbol == key.F2:
-            print("Projection: 3D Isometric")
             self.mode = 2
             self.isometric()
 
@@ -55,6 +117,24 @@ class Camera(object):
             self.fov += 1
             self.perspective()
 
+        elif symbol == key.LEFT:
+            cube_translations[1][0] -= 5.0
+
+        elif symbol == key.RIGHT:
+            cube_translations[1][0] += 5.0
+
+        elif symbol == key.UP:
+            cube_translations[1][2] -= 5.0
+
+        elif symbol == key.DOWN:
+            cube_translations[1][2] += 5.0
+
+        elif symbol == key.SPACE:
+            cube_translations[1][1] += 5.0
+
+        elif symbol == key.LSHIFT:
+            cube_translations[1][1] -= 5.0
+
     def drag(self, x, y, dx, dy, button, modifiers):
         if button == 1:
             self.x -= dx * 2
@@ -65,7 +145,7 @@ class Camera(object):
             self.rx -= dy / 4
 
     def scroll(self, x, y, scroll_x, scroll_y):
-            self.z -= scroll_y * 10
+        self.z -= scroll_y * 10
 
     def apply(self):
         glLoadIdentity()
@@ -75,72 +155,31 @@ class Camera(object):
         glRotatef(self.rz, 0, 0, 1)
 
 
-def init():
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glDepthFunc(GL_LEQUAL)
-
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    glClearDepth(1.0)
-
-    glEnable(GL_DEPTH_TEST)
-    glDepthFunc(GL_LEQUAL)
-    glShadeModel(GL_SMOOTH)
-    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST)
-
-    pyglet.app.run()
-
-
-def draw_cube(pos, scale, color):
+def add_cube(id, pos, scale, color):
     x, y, z = pos
     w, h, d = scale
     r, g, b, a = color
 
-    glBegin(GL_QUADS)
-    glColor4f(r, g, b, a)
+    vertices = []
+    colors = []
 
-    # Top
-    glVertex3f(x - w, y + h, z - d)
-    glVertex3f(x - w, y + h, z + d)
-    glVertex3f(x + w, y + h, z + d)
-    glVertex3f(x + w, y + h, z - d)
+    for sx, sy, sz in cube_signs:
+        vertices.extend([x + (w * sx), y + (h * sy), z + (d * sz)])
+        colors.extend([r, g, b, a])
 
-    # Bottom
-    glVertex3f(x - w, y - h, z - d)
-    glVertex3f(x + w, y - h, z - d)
-    glVertex3f(x + w, y - h, z + d)
-    glVertex3f(x - w, y - h, z + d)
+    cube_indexes[id] = [(len(cube_vertices), len(cube_vertices) + len(vertices)),
+                        (len(cube_colors), len(cube_colors) + len(colors))]
+    cube_vertices.extend(vertices)
+    cube_colors.extend(colors)
 
-    # Left
-    glVertex3f(x - w, y - h, z - d)
-    glVertex3f(x - w, y - h, z + d)
-    glVertex3f(x - w, y + h, z + d)
-    glVertex3f(x - w, y + h, z - d)
-
-    # Right
-    glVertex3f(x + w, y - h, z + d)
-    glVertex3f(x + w, y - h, z - d)
-    glVertex3f(x + w, y + h, z - d)
-    glVertex3f(x + w, y + h, z + d)
-
-    # Front
-    glVertex3f(x - w, y - h, z + d)
-    glVertex3f(x + w, y - h, z + d)
-    glVertex3f(x + w, y + h, z + d)
-    glVertex3f(x - w, y + h, z + d)
-
-    # Back
-    glVertex3f(x + w, y - h, z - d)
-    glVertex3f(x - w, y - h, z - d)
-    glVertex3f(x - w, y + h, z - d)
-    glVertex3f(x + w, y + h, z - d)
-
-    glEnd()
+    cube_translations[id] = [0.0, 0.0, 0.0]
 
 
 class CameraWindow(pyglet.window.Window):
     def __init__(self):
         super(CameraWindow, self).__init__(resizable=True)
+        self.maximize()
+
         self.cam = Camera()
         self.on_resize = self.cam.view
         self.on_key_press = self.cam.key
@@ -152,9 +191,19 @@ class CameraWindow(pyglet.window.Window):
         self.cam.apply()
         fps_display.draw()
 
-        for x in range(100):
-            draw_cube((x, 1, 1), (1, 1, 1), (1.0, 0.0, 0.0, 1.0))
-        # draw_cube((10, 1, 1), (2, 2, 2), (1.0, 0.0, 0.0, 0.1))
+        glEnable(GL_DEPTH_TEST)
 
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_COLOR_ARRAY)
+
+        vertex_vbo.vertex()
+        color_vbo.color()
+
+        for cube in cube_indexes:
+            glTranslatef(*cube_translations[cube])
+            glDrawArrays(GL_QUADS, cube_indexes[cube][0][0], cube_indexes[cube][0][1])
+            glTranslatef(-cube_translations[cube][0], -cube_translations[cube][1], -cube_translations[cube][2])
+
+init()
 window = CameraWindow()
 pyglet.app.run()
